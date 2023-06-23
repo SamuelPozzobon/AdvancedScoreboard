dce41f189a3e1553a7d1327aadb1996ff55a722e
Maverick <42522736+SamuelPozzobon@users.noreply.github.com>
2023-06-23 23:06:47 +0200

    fixed tag ITEM_ID

--- src/varion/AdvancedScoreboard.php
+++ src/varion/AdvancedScoreboard.php
@@ -110,7 +110,7 @@ class AdvancedScoreboard extends PluginBase{
                }
               }
                 else{
-                    $sender->sendMessage(TF ::RED . "Use this Command in-game.");
+                    $sender->sendMessage(TF::RED . "Use this Command in-game.");
                     return true;
                 }
                     }
@@ -184,7 +184,7 @@ class AdvancedScoreboard extends PluginBase{
         $message = str_replace('{Z}', $player->getPosition()->getZ(), $message);
 
         $message = str_replace('{IP}', $player->getNetworkSession()->getIP(), $message);
-        // $message = str_replace('{ITEM_ID}', $player->getInventory()->getItemInHand(), $message);
+        $message = str_replace('{ITEM_ID}', $player->getInventory()->getItemInHand(), $message);
         $level = $player->getWorld();
         $message = str_replace('{WORLDNAME}', $level->getFolderName(), $message);
         $message = str_replace('{WORLDPLAYERS}', count($level->getPlayers()), $message);
